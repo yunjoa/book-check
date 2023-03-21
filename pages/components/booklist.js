5ba3d00d0ace93ee723a807b8d2a6da6d09298bd
jihwa <mungsome@gmail.com>
2023-03-21 16:28:38 +0900

    && 대체법

--- pages/components/booklist.js
+++ pages/components/booklist.js
@@ -15,7 +15,16 @@ export default function Booklist({ abook }) {
         className="flex max-md:flex-col flex-row w-full p-10 border border-4 border-black items-center max-md:items-center group  mb-4 dark:border-white"
       >
         <p className=" w-4/5 text-ellipsis break-keep text-xl pr-10 max-md:pr-0 group-hover:underline dark:group-hover:decoration-orange-400 underline-offset-4 decoration-emerald-400  ">
-          {abook && abook.properties.Name.title[0].plain_text}
+          {abook && abook.properties.Name.title[0].plain_text
+            ? abook.properties.Name.title[0].plain_text
+            : ""}
+          {/* {abook &&
+          abook.properties &&
+          abook.properties.Name &&
+          abook.properties.Name.title &&
+          abook.properties.Name.title.length > 0
+            ? abook.properties.Name.title[0].plain_text
+            : ""} */}
         </p>
         <div className="max-md:mt-10 max-md:w-full  w-1/5 mt-0 text-left line-2 ">
           <p className="text-sm leading-loose truncate ;">
