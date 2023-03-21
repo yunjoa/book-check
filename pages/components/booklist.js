f087bf94f9fee71293d0dc5aabccde8b3cd25f45
jihwa <mungsome@gmail.com>
2023-03-21 16:45:08 +0900

    prop test

--- pages/components/booklist.js
+++ pages/components/booklist.js
@@ -8,6 +8,7 @@ export default function Booklist({ value }) {
     setModalOn(true);
     setIsOpen(true);
   };
+  // console.log(value.properties.Name.title[0].plain_text);
   return (
     <>
       <div
@@ -15,20 +16,20 @@ export default function Booklist({ value }) {
         className="flex max-md:flex-col flex-row w-full p-10 border border-4 border-black items-center max-md:items-center group  mb-4 dark:border-white"
       >
         <p className=" w-4/5 text-ellipsis break-keep text-xl pr-10 max-md:pr-0 group-hover:underline dark:group-hover:decoration-orange-400 underline-offset-4 decoration-emerald-400  ">
-          {/* {abook && abook.properties.Name.title[0].plain_text
-            ? abook.properties.Name.title[0].plain_text
-            : ""} */}
-          {/* {abook &&
-          abook.properties &&
-          abook.properties.Name &&
-          abook.properties.Name.title &&
-          abook.properties.Name.title.length > 0
-            ? abook.properties.Name.title[0].plain_text
+          {value && value.properties.Name.title[0].plain_text
+            ? value.properties.Name.title[0].plain_text
+            : ""}
+          {/* {value &&
+          value.properties &&
+          value.properties.Name &&
+          value.properties.Name.title &&
+          value.properties.Name.title.length > 0
+            ? value.properties.Name.title[0].plain_text
             : ""} */}
         </p>
         <div className="max-md:mt-10 max-md:w-full  w-1/5 mt-0 text-left line-2 ">
           <p className="text-sm leading-loose truncate ;">
-            {/* {abook&&abook.properties.Author.id} */}
+            {/* {value&&value.properties.Author.id} */}
             작가
           </p>
           <p className="text-sm leading-loose truncate ;">출판사</p>
@@ -41,7 +42,7 @@ export default function Booklist({ value }) {
             setModalOn(modalOn == true);
           }}
         ></div>
-        {/* <Modal abook={abook} open={isOpen} /> */}
+        {/* <Modal abook={value} open={isOpen} /> */}
       </>
     </>
   );
