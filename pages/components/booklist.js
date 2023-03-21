3a78f3e05de9ada222f33d24561e39bae61846ae
jihwa <mungsome@gmail.com>
2023-03-21 16:26:57 +0900

    && 추가

--- pages/components/booklist.js
+++ pages/components/booklist.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 // import Modal from "./modal";
 
-export default function Booklist({ data }) {
+export default function Booklist({ abook }) {
   const [modalOn, setModalOn] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
   const clickBook = () => {
@@ -15,11 +15,11 @@ export default function Booklist({ data }) {
         className="flex max-md:flex-col flex-row w-full p-10 border border-4 border-black items-center max-md:items-center group  mb-4 dark:border-white"
       >
         <p className=" w-4/5 text-ellipsis break-keep text-xl pr-10 max-md:pr-0 group-hover:underline dark:group-hover:decoration-orange-400 underline-offset-4 decoration-emerald-400  ">
-          {data.properties.Name.title[0].plain_text}
+          {abook && abook.properties.Name.title[0].plain_text}
         </p>
         <div className="max-md:mt-10 max-md:w-full  w-1/5 mt-0 text-left line-2 ">
           <p className="text-sm leading-loose truncate ;">
-            {/* {data.properties.Author.id} */}
+            {/* {abook&&abook.properties.Author.id} */}
             작가
           </p>
           <p className="text-sm leading-loose truncate ;">출판사</p>
@@ -32,7 +32,7 @@ export default function Booklist({ data }) {
             setModalOn(modalOn == true);
           }}
         ></div>
-        {/* <Modal data={data} open={isOpen} /> */}
+        {/* <Modal abook={abook} open={isOpen} /> */}
       </>
     </>
   );
